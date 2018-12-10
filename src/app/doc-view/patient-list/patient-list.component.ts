9da166c94f3e44477b8a0c0f2f9b7bb5e67e3949
Tobias <sa16b024@technkum-wien.at>
2018-12-10 02:57:41 +0100

    chore(all): add function

--- src/app/doc-view/patient-list/patient-list.component.ts
+++ src/app/doc-view/patient-list/patient-list.component.ts
@@ -1,51 +1,97 @@
 import {Component, OnInit, ViewChild, ViewChildren} from '@angular/core';
 import {PATIENTS} from '../../mock-files/mock-patients';
 import {Patient} from '../../mock-files/patients';
+import {DATES} from '../../mock-files/mock-vital-parameter';
 
 @Component({
-  selector: 'app-patient-list',
-  templateUrl: './patient-list.component.html',
-  styleUrls: ['./patient-list.component.scss']
+    selector: 'app-patient-list',
+    templateUrl: './patient-list.component.html',
+    styleUrls: ['./patient-list.component.scss']
 })
 
 export class PatientListComponent implements OnInit {
-  patients = PATIENTS;
-  selectedPatient: Patient;
-  @ViewChild('generalSection') generalSection;
-  @ViewChild('newPatientSection') newPatientSection;
-  @ViewChild('detailSection') detailSection;
+    patients = PATIENTS;
+    selectedPatient: Patient;
+    dates = DATES;
 
-  @ViewChild('changeOn') changeOn;
-  @ViewChild('changeOff') changeOff;
+    @ViewChild('generalSection') generalSection;
+    @ViewChild('newPatientSection') newPatientSection;
+    @ViewChild('detailSection') detailSection;
 
-  @ViewChild('changeAge') changeAge;
-  @ViewChild('changeLastVisit') changeLastVisit;
-  @ViewChild('changeCurrentMedication') changeCurrentMedication;
-  @ViewChild('changeMedicationInterval') changeMedicationInterval;
-  @ViewChild('changeUsedMedication') changeUsedMedication;
+    @ViewChild('changeOn') changeOn;
+    @ViewChild('changeOff') changeOff;
+    @ViewChild('saveBtn') saveBtn;
 
-  constructor() { }
+    @ViewChild('changeAge') changeAge;
+    @ViewChild('changeLastVisit') changeLastVisit;
+    @ViewChild('changeCurrentMedication') changeCurrentMedication;
+    @ViewChild('changeMedicationInterval') changeMedicationInterval;
+    @ViewChild('changeUsedMedication') changeUsedMedication;
 
-  ngOnInit() {
-  }
+    @ViewChild('sv') sv;
+    @ViewChild('firstName') firstName;
+    @ViewChild('lastName') lastName;
+    @ViewChild('age') age;
+    @ViewChild('gender') gender;
+    @ViewChild('lastVisit') lastVisit;
+    @ViewChild('registered') registered;
+    @ViewChild('currentMedication') currentMedication;
+    @ViewChild('medicationInterval') medicationInterval;
+    @ViewChild('usedMedication') usedMedication;
 
-  onSelect(patient: Patient): void {
-    this.selectedPatient = patient;
-    this.generalSection.nativeElement.style.display = 'none';
-    this.detailSection.nativeElement.style.display = 'block';
-  }
-  onAddBtn(): void {
-    this.newPatientSection.nativeElement.style.display = 'block';
-    this.generalSection.nativeElement.style.display = 'none';
-  }
-  onBackBtn(): void {
-    this.generalSection.nativeElement.style.display = 'block';
-    this.newPatientSection.nativeElement.style.display = 'none';
-    this.detailSection.nativeElement.style.display = 'none';
-  }
-  onChangeBtn(): void{
+    constructor() { }
+
+    ngOnInit() {
+    }
+
+    onSelect(patient: Patient): void {
+        this.selectedPatient = patient;
+        this.generalSection.nativeElement.style.display = 'none';
+        this.detailSection.nativeElement.style.display = 'block';
+    }
+    onAddBtn(): void {
+        this.newPatientSection.nativeElement.style.display = 'block';
+        this.generalSection.nativeElement.style.display = 'none';
+    }
+    onAddPatientBtn(): void {
+        const sv = this.sv.nativeElement.value;
+        const firstName = this.firstName.nativeElement.value;
+        const lastName = this.lastName.nativeElement.value;
+        const age = this.age.nativeElement.value;
+        const gender = this.gender.nativeElement.value;
+        const lastVisit = this.lastVisit.nativeElement.value;
+        const registered = this.registered.nativeElement.value;
+        const currentMedication = this.currentMedication.nativeElement.value;
+        const medicationInterval = this.medicationInterval.nativeElement.value;
+        const usedMedication = this.usedMedication.nativeElement.value;
+        const tmp: Patient = {
+            sv: sv,
+            first_name: firstName,
+            last_name: lastName,
+            gender: gender,
+            age: age,
+            registered: registered,
+            last_visit: lastVisit,
+            current_medication: currentMedication,
+            medication_interval: medicationInterval,
+            used_medication: usedMedication
+        };
+        PATIENTS.unshift(tmp);
+
+        this.generalSection.nativeElement.style.display = 'block';
+        this.newPatientSection.nativeElement.style.display = 'none';
+        this.detailSection.nativeElement.style.display = 'none';
+    }
+
+    onBackBtn(): void {
+        this.generalSection.nativeElement.style.display = 'block';
+        this.newPatientSection.nativeElement.style.display = 'none';
+        this.detailSection.nativeElement.style.display = 'none';
+    }
+    onChangeBtn(): void{
         this.changeOff.nativeElement.style.display = 'block';
         this.changeOn.nativeElement.style.display = 'none';
+        this.saveBtn.nativeElement.style.display = 'block';
         this.changeAge.nativeElement.disabled = false;
         this.changeLastVisit.nativeElement.disabled = false;
         this.changeCurrentMedication.nativeElement.disabled = false;
@@ -55,6 +101,7 @@ export class PatientListComponent implements OnInit {
     offChangeBtn(): void{
         this.changeOff.nativeElement.style.display = 'none';
         this.changeOn.nativeElement.style.display = 'block';
+        this.saveBtn.nativeElement.style.display = 'none';
         this.changeAge.nativeElement.disabled = true;
         this.changeLastVisit.nativeElement.disabled = true;
         this.changeCurrentMedication.nativeElement.disabled = true;
--- src/app/mock-files/patients.ts
+++ src/app/mock-files/patients.ts
@@ -1,5 +1,3 @@
-import {PATIENTS} from './mock-patients';
-
 export class Patient {
     sv: number;
     first_name: string;
@@ -12,12 +10,3 @@ export class Patient {
     medication_interval: string;
     used_medication: string;
 }
-
-/*export function createPatient(sv: number, first_name: string, last_name: string, gender: string, age: number, registered: string,
-                              last_visit: string, current_medication: string, medication_interval: string, used_medication: string) {
-
-    let newPatient = 'sv: ' + sv + ', first_name: "' + first_name + '", last_name: "' + last_name + '", gender: "' + gender +
-    '", age: ' + age + ', registered: "' + registered + '", last_visit: "' + last_visit + '", current_medication: "' +
-    current_medication + '", medication_interval: "' + medication_interval + '", used_medication: "' + used_medication + '"';
-    PATIENTS.push('{' + newPatient + '}');
-};*/
