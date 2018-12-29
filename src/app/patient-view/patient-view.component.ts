0f7ff5107d3f75b5dd46915fe0de4bd346c44fef
Tobias <sa16b024@technkum-wien.at>
2018-12-29 02:47:09 +0100

    chore(patient): requiered-elements

--- src/app/patient-view/patient-view.component.ts
+++ src/app/patient-view/patient-view.component.ts
@@ -9,9 +9,9 @@ const systoleValues = [];
 const diastoleValues = [];
 const heartRateValues = [];
 DATES.forEach(function (value) {
-    systoleValues.unshift([value.timestamp * 1000 , value.systole]);
-    diastoleValues.unshift([value.timestamp * 1000 , value.diastole]);
-    heartRateValues.unshift([value.timestamp * 1000 , value.heartbeat]);
+    systoleValues.push([value.timestamp * 1000 , value.systole]);
+    diastoleValues.push([value.timestamp * 1000 , value.diastole]);
+    heartRateValues.push([value.timestamp * 1000 , value.heartbeat]);
 });
 
 
