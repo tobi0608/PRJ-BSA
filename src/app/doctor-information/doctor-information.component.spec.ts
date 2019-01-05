43818c4048656215fb1a6b38df9e13a21c1dc297
Tobias <sa16b024@technkum-wien.at>
2019-01-05 13:49:42 +0100

    chore(pat): finish style

---
+++ src/app/doctor-information/doctor-information.component.spec.ts
@@ -0,0 +1,25 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+
+import { DoctorInformationComponent } from './doctor-information.component';
+
+describe('DoctorInformationComponent', () => {
+  let component: DoctorInformationComponent;
+  let fixture: ComponentFixture<DoctorInformationComponent>;
+
+  beforeEach(async(() => {
+    TestBed.configureTestingModule({
+      declarations: [ DoctorInformationComponent ]
+    })
+    .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(DoctorInformationComponent);
+    component = fixture.componentInstance;
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
---
+++ src/app/doctor-information/doctor-information.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-doctor-information',
+  templateUrl: './doctor-information.component.html',
+  styleUrls: ['./doctor-information.component.scss']
+})
+export class DoctorInformationComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
